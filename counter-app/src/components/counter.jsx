67329ed7eddf8a936ca8a749f721058d30644368
hypnoticpenguin <chankevin234@gmail.com>
2020-12-06 19:58:24 -0500

    updated with list rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = { //constructor
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+    };//data this component needs
+    
+    styles = {
+        fontSize: 1000,
+        //fontWeight: "bold"
+    };
+
     render() {
-        return <h1>Hello World</h1>; //h1 element
+        let classes = this.getBadgeClasses(); 
+        
+        return (
+            <div>
+                <span style={ this.styles } className={ this.getBadgeClasses() }>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li>{ tag }</li>) }
+                </ul>
+            </div> //h1 element
+        );    
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
     }
 }
 
